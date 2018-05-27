30ed9dd63791339b7e73ec5de64c19fecd14beef
kaypler <dxuwen@gmail.com>
2018-05-27 16:53:25 +0800

    tree sharking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    var element = document.createElement('pre');
 
-    // Lodash, now imported by this scrpt
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+    element.innerHTML = [
+        'Hello Webpack',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
